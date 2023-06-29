2e07733ca9a66f641f73a2c452a8300d852aa6a0
FrhCode <farhan7534031b@gmail.com>
2023-06-29 21:23:11 +0700

    first commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $category = Category::all();
+        return [
+            "title" => $this->faker->text(20),
+            "author" => $this->faker->name(),
+            'category_id' => $category->random()->id
+        ];
+    }
+}
